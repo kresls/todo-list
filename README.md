c858295b61f4036320ae42f1dfa17fa094da8984
Filip Zalamans <filip.zalamans@gmail.com>
2020-11-19 23:13:02 +0100

    Add essentially all functionality and styling

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # To-do list
 
 - Built in JQuery
+- Using JQuery UI Selectable Widget
+
+## Features to add
+
+- Dragging and dropping on touch screens
--- js/main.js
+++ js/main.js
@@ -2,59 +2,72 @@ const list = "#list",
       listItem = ".list-item",
       form = "#add-item";
 
-function InsertTestingItems() {
-  $(list).append(CreateListItem("Köpa pennor"));
-  $(list).append(CreateListItem("Gå på promenad"));
-  $(list).append(CreateListItem("Svara på frågor på Stack Overflow"));
-  $(list).append(CreateListItem("Hjälp till i GitHub-projekt"));
-}
-
-function CreateListItem(text) {
+// Helper function to create a list item
+// Returns a string with html code
+function CreateListItem(str) {
   return  "<div class='list-item'>"
           + "<span class='item-number'></span>"
-          + "<span class='item-text'>" + text + "</span>"
-          + "<button class='btn-move-up'>⋀</button>"
-          + "<button class='btn-move-down'>⋁</button>"
-          + "<button class='btn-del'>X</button>"
+          + "<span class='item-text'>" + str + "</span>"
+          + "<button class='btn-move-up' title='Move the item one step up'>⋀</button>"
+          + "<button class='btn-move-down' title='Move the item one step down'>⋁</button>"
+          + "<button class='btn-del' title='Remove the item'>X</button>"
           + "</div>";
 }
 
+// Helper function to keep list numbered
 function UpdateListNumbers() {
   $(list).children().each(function(li) {
     $(this).find(".item-number").text((li + 1) + ".");
   });
 }
 
+function MoveItemUp(item) {
+  item.insertBefore(item.prev());
+}
+
+function MoveItemDown(item) {
+  item.insertAfter(item.next());
+}
+
 $(document).ready(function() {
-  InsertTestingItems();
-  UpdateListNumbers();
 
   // Form for adding a to-do
   $(form).submit(function(e) {
     e.preventDefault();
-    let text = $(form).children("input[type=text]").val();
-    if(text.trim() !== "") {
-      $(list).append(CreateListItem(text));
+    let input = $(form).children("input[type=text]").val();
+    if(input.trim() !== "") {
+      $(list).append(CreateListItem(input));
       $(form).children("input[type=text]").val("");
       UpdateListNumbers();
     }
   });
 
-  // Use existing element for click event handler since list items are created dynamically
+  // Use existing element for click event handlers since list items are created dynamically
   $(list).on("click", "button", function() {
     if($(this).hasClass("btn-del")) { // When button with class .btn-del is clicked
       $(this).parent().remove();
       UpdateListNumbers();
     }
     else if($(this).hasClass("btn-move-up")) { // When button with class .btn-move-up is clicked
-      $(this).parent().insertBefore($(this).parent().prev());
+      MoveItemUp($(this).parent());
       UpdateListNumbers();
     }
     else if($(this).hasClass("btn-move-down")) { // When button with class .btn-move-down is clicked
-      $(this).parent().insertAfter($(this).parent().next());
+      MoveItemDown($(this).parent())
       UpdateListNumbers();
     }
   });
 
+  // Use Sortable Widget on the list
+  // Call UpdateListNumbers() when an item has been dropped
+  $(list).sortable({
+    update: function(e, ui) {
+      UpdateListNumbers();
+    }
+  });
+
+  $("#btn-toggle-info").click(function() {
+    $(".info").toggleClass("show");
+  });
 
 });
